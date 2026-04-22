24d053ece8b13c2fc9db66486a5f8c150ca1b2ae
Sowjanya <sowjanyaranjith24@gmail.com>
2026-04-22 13:58:12 +0530

    simplify cors

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir, { recursive: true });
 if (!fs.existsSync(outputsDir)) fs.mkdirSync(outputsDir, { recursive: true });
 
 // Middleware
-app.use(cors({ origin: '*', methods: ['GET', 'POST', 'DELETE'] }));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
